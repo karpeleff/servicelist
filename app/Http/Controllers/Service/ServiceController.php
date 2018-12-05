4cddb7b57f79f5e85b8af1de40c6e48f63b21590
karpeleff <karpelev1976@gmail.com>
2018-12-05 11:11:29 +1000

    add  model service

--- app/Http/Controllers/Service/ServiceController.php
+++ app/Http/Controllers/Service/ServiceController.php
@@ -4,9 +4,20 @@ namespace App\Http\Controllers\Service;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Oprs\Models\Service\Service;
 
 class ServiceController extends Controller
 {
+    
+     protected $request;
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+    
+    
+    
+    
    public function index()
    {
      return view('service.index');
